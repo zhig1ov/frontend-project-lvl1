b606c083eebee781e0dfc6658e1dc4461662ba09
Artem <zhiglov.artem@yandex.ru>
2021-12-26 00:59:58 +0500

    connected the readline-sync library

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync'
+
 console.log('Welcome to the Brain Games!')
+
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
